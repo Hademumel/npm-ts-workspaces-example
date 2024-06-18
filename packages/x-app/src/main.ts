b6f12749bc3abd3efccb763e7e2871691deaeb9e
Rolf Ripszam <rolf.ripszam@mgm-tp.com>
2024-06-18 17:37:12 +0200

    customize forked project

--- packages/x-app/src/main.ts
+++ packages/x-app/src/main.ts
@@ -1,9 +1,9 @@
 import { awesomeFn } from "@quramy/x-core";
 import { add } from "@quramy/x-core/src/math";
 
-export async function main() {
+export function main() {
   // dependencies across child packages
-  const out = await awesomeFn();
+  const out = awesomeFn();
   console.log(add(1, 1));
   return out;
 }
